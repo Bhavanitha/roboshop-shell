a01b68a7f7b4fa97c054f995fa7ecef34328f31a
Bhavana <vbhavanitha@gmail.com>
2023-08-28 13:42:06 +0530

    Roboshop nginx script_updated

--- catalogue.sh
+++ catalogue.sh
@@ -1,19 +1,17 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
-yum install nodejs -y &>>/tmp/roboshop.log
-useradd roboshop &>>/tmp/roboshop.log
-rm -rf /app &>>/tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+rm -rf /app
 mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip
 cd /app
-npm install &>>/tmp/roboshop.log
-cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl start catalogue &>>/tmp/roboshop.log
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
-yum install mongodb-org-shell -y &>>/tmp/roboshop.log
-mongo --host mongodb-dev.bhavanitha.site </app/schema/catalogue.js &>>/tmp/roboshop.log
-
-
+npm install
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org-shell -y
+mongo --host mongodb-dev.bhavanitha.site </app/schema/catalogue.js
