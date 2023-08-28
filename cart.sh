3a47e0b311187499aa6dfb9c99633a05a6456a60
Bhavana <vbhavanitha@gmail.com>
2023-08-28 22:48:29 +0530

    Cart Script

--- cart.sh
+++ cart.sh
@@ -1,33 +1,33 @@
 echo -e "\e[32m configuring nodejs \e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
 echo -e "\e[32m install nodejs \e[0m"
-yum install nodejs -y
+yum install nodejs -y &>>/tmp/roboshop.log
 
 echo -e "\e[32m add application user \e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[32m create application directory \e[0m"
-rm -rf /app
+rm -rf /app &>>/tmp/roboshop.log
 mkdir /app
 
 echo -e "\e[32m downloading Cart content\e[0m"
-curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>>/tmp/roboshop.log
 
 echo -e "\e[32m extract application content\e[0m"
 cd /app
-unzip /tmp/cart.zip
+unzip /tmp/cart.zip &>>/tmp/roboshop.log
 
 echo -e "\e[32m  install nodejs dependencies\e[0m"
 cd /app
-npm install
+npm install &>>/tmp/roboshop.log
 
 echo -e "\e[32m configure systemd services\e[0m"
 #cd /home/centos/roboshop-shell
-cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>>/tmp/roboshop.log
 
 echo -e "\e[32m  start user service\e[0m"
-systemctl daemon-reload
-systemctl enable cart
-systemctl restart cart
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable cart &>>/tmp/roboshop.log
+systemctl restart cart &>>/tmp/roboshop.log
 
